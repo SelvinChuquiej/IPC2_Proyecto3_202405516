ea482db0611b709b4ff2f4f9c15e5bf9a6e913ec
Selvin Raúl Chuquiej Andrade <schuquiej.a@gmail.com>
2025-10-21 23:45:29 -0600

    Implementacion de Front para consumos XML

--- IPC2_Proyecto3_202405516/Front/view/app/urls.py
+++ IPC2_Proyecto3_202405516/Front/view/app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('enviar_configuracion/', views.enviar_configuracion, name='enviar_configuracion'),
+    path('enviar_consumos/', views.enviar_consumos, name='enviar_consumos'),
 ]
--- IPC2_Proyecto3_202405516/Front/view/app/views.py
+++ IPC2_Proyecto3_202405516/Front/view/app/views.py
@@ -10,10 +10,10 @@ def home(request):
 
 def enviar_configuracion(request):
     if request.method == 'POST':
-        if 'xml_file' not in request.FILES:
+        if 'xml_file_config' not in request.FILES:
             messages.error(request, "No se ha seleccionado ningún archivo.")
             return redirect('home') 
-        uploaded = request.FILES['xml_file']
+        uploaded = request.FILES['xml_file_config']
 
         try:
             file_bytes = uploaded.read().decode('utf-8') 
@@ -29,6 +29,34 @@ def enviar_configuracion(request):
             resp = requests.post(f"{API_URL}/api/configuracion", data=file_bytes, headers=headers, timeout=15)
             if resp.status_code == 200:
                 messages.success(request, f'XML procesado correctamente: {resp.text}')
+            else:
+                messages.error(request, f'Error del backend: {resp.status_code} - {resp.text}')
+        except requests.RequestException as e:
+            messages.error(request, f'Error de conexión con el backend: {e}')
+
+    return render(request, 'index.html')
+
+def enviar_consumos(request):
+    if request.method == 'POST':
+        if 'xml_file_consumos' not in request.FILES:
+            messages.error(request, "No se ha seleccionado ningún archivo.")
+            return redirect('home') 
+        uploaded = request.FILES['xml_file_consumos']
+
+        try:
+            file_bytes = uploaded.read().decode('utf-8') 
+            if not file_bytes.strip().startswith('<?xml'):
+                messages.error(request, "El archivo no es un XML válido.")
+                return redirect('home')
+        except Exception as e:
+            messages.error(request, f"Error al leer el archivo: {str(e)}")
+            return redirect('home')
+        
+        try:
+            headers = {'Content-Type': 'application/xml'}
+            resp = requests.post(f"{API_URL}/api/consumos", data=file_bytes, headers=headers, timeout=15)
+            if resp.status_code == 200:
+                messages.success(request, f'XML de consumos procesado correctamente: {resp.text}')
             else:
                 messages.error(request, f'Error del backend: {resp.status_code} - {resp.text}')
         except requests.RequestException as e:
